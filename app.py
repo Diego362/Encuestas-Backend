103f7188a394d9bb56577fdcacfdd427c022a167
Diego Cabrera <diego362a@gmail.com>
2021-11-23 16:16:40 -0500

    'cambios'

--- app.py
+++ app.py
@@ -1,315 +1,372 @@
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS
 import mysql.connector
+from flask_jwt_extended import (
+    create_access_token,
+    JWTManager,
+    jwt_required,
+    get_jwt_identity,
+)
 
 db = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password ='',
-    database = 'sistema',
-    port =3306
-    
+    host="localhost", user="root", password="", database="sistema", port=3306
 )
 
 app = Flask(__name__)
 CORS(app)
+app.config["JWT_SECRET_KEY"] = "nMBsZ&pP@Bc@6VzVAqahzZnC!JrZBHbf"
+jwt = JWTManager(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Hello world'
+    return "Hello world"
 
 
-
-@app.post('/usuarios')
+@app.post("/usuarios")
 def crearUsuario():
-    #request  => envia el cliente
-    #response => lo que le voy a responder
+    # request  => envia el cliente
+    # response => lo que le voy a responder
     datos = request.json
-    
+
     print(datos)
 
     cursor = db.cursor()
 
-    cursor.execute('''INSERT INTO usuario(nombre, correo, contraseña)
-        VALUE(%s, %s, %s)''', (
-        datos['Nombre'],
-        datos['Correo'],
-        datos['Contraseña'],
-    ))
+    cursor.execute(
+        """INSERT INTO usuario(nombre, correo, contraseña)
+        VALUE(%s, %s, %s)""",
+        (
+            datos["Nombre"],
+            datos["Correo"],
+            datos["Contraseña"],
+        ),
+    )
 
     db.commit()
-    
-    return jsonify({
 
-        "mensaje": "usuario almacenado correctamente"
-    })
+    return jsonify({"mensaje": "usuario almacenado correctamente"})
 
 
-@app.get('/usuarios')
+@app.post("/login")
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    cursor = db.cursor(dictionary=True)
+    cursor.execute(
+        """select * from usuario where correo = %s and contraseña = %s """,
+        (
+            email,
+            password,
+        ),
+    )
+
+    usuario = cursor.fetchone()
+
+    cursor.close
+
+    if not usuario:
+        return jsonify({"message": "Datos de acceso invalidos"})
+
+    token = create_access_token(identity=usuario["id_usuario"])
+
+    return jsonify({"token": token})
+
+
+@app.get("/usuarios")
+@jwt_required()
 def listaUsuarios():
+    usuario = get_jwt_identity()
+    print(usuario)
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute('select * from usuario')
+    cursor.execute("select * from usuario")
 
     registros = cursor.fetchall()
 
     return jsonify(registros)
 
 
-
-
-
-@app.put('/usuarios/<id>')
+@app.put("/usuarios/<id>")
 def actualizarUsuario(id):
 
-    datos=request.json
+    datos = request.json
 
     cursor = db.cursor()
 
+    cursor.execute(
+        """UPDATE usuario set nombre=%s, 
+        correo=%s, contraseña=%s where id_usuario=%s""",
+        (datos["Nombre"], datos["Correo"], datos["Contraseña"], id),
+    )
 
-    cursor.execute('''UPDATE usuario set nombre=%s, 
-        correo=%s, contraseña=%s where id_usuario=%s''',(
-            datos['Nombre'],
-            datos['Correo'],
-            datos['Contraseña'],
-            id
-        ))
-    
     db.commit()
 
-    return jsonify({
+    return jsonify({"mensaje": "usuario actualizado correctamente"})
 
-        "mensaje": "usuario actualizado correctamente"
-    })  
-    
-    
-@app.delete('/usuarios/<id>')
-def eliminarUsuario(id):
 
+@app.delete("/usuarios/<id_usuario>")
+def eliminarUsuario(id_usuario):
 
     cursor = db.cursor()
-    cursor.execute('DELETE FROM usuario where id_usuario=%s',(id))
-
+    cursor.execute(
+        "DELETE FROM usuario WHERE id_usuario=%s",
+        (id_usuario,),
+    )
 
     db.commit()
 
-    return jsonify({
+    return jsonify({"mensaje": "usuario eliminado correctamente"})
 
-        "mensaje": "usuario eliminado correctamente"
-    })
 
-@app.get('/usuarios/<id>')
+@app.get("/usuarios/<id>")
 def unUsuario(id):
-    cursor = db.cursor()
-    cursor.execute('SELECT * FROM usuario where id_usuario=%s',[id])
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM usuario where id_usuario=%s", [id])
 
-    usuario = cursor.fetchall()
-    
+    usuario = cursor.fetchone()
+
     return jsonify(usuario)
 
-@app.post('/usuarios/<id_usuario>/encuestas')
-def crearEncuesta(id_usuario):
-    #request  => envia el cliente
-    #response => lo que le voy a responder
+
+@app.post("/encuestas")
+def crearEncuesta():
+    # request  => envia el cliente
+    # response => lo que le voy a responder
     datos = request.json
-    
+
     cursor = db.cursor()
 
-    cursor.execute('''INSERT INTO encuesta(nombre, id_usuario)
-        VALUE(%s, %s)''', (
-        datos['Nombre'],
-        id_usuario,
-    ))
+    cursor.execute(
+        """INSERT INTO encuesta(nombre, id_usuario)
+        VALUE(%s, %s)""",
+        (
+            datos["Nombre"],
+            datos["id_usuario"],
+        ),
+    )
 
     db.commit()
-    
-    return jsonify({
 
-        "mensaje": "encuesta almacenada correctamente"
-    })
-    
-@app.get('/usuarios/encuestas')
+    return jsonify({"mensaje": "encuesta almacenada correctamente"})
+
+
+@app.get("/encuestas")
 def listaEncuesta():
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute('select * from encuesta')
+    cursor.execute("select * from encuesta")
 
     registros = cursor.fetchall()
 
     return jsonify(registros)
 
-@app.put('/usuarios/<id_usuario>/encuestas/<id_encuesta>')
-def actualizarEncuesta(id_usuario, id_encuesta):
 
-    datos=request.json
+@app.put("/encuestas/<id_encuesta>")
+def actualizarEncuesta(id_encuesta):
 
-    cursor = db.cursor()
-
-
-    cursor.execute('''UPDATE encuesta set nombre=%s, id_usuario=%s
-        where id_encuesta=%s''',(
-            datos['Nombre'],
-            id_usuario,
-            id_encuesta
-        ))
-    
-    db.commit()
-
-    return jsonify({
-
-        "mensaje": "encuesta actualizada correctamente"
-    })  
-
-@app.delete('/usuarios/<id_usuario>/encuestas/<id_encuesta>')
-def eliminarEncuesta(id_usuario, id_encuesta):
-
-
-    cursor = db.cursor()
-    cursor.execute('DELETE FROM encuesta where id_usuario=%s AND id_encuesta=%s',(id_usuario, id_encuesta))
-
-
-    db.commit()
-
-    return jsonify({
-
-        "mensaje": "encuesta eliminada correctamente"
-    })
-
-@app.post('/encuestas/<id_encuesta>/secciones')
-def crearSeccion(id_encuesta):
-    #request  => envia el cliente
-    #response => lo que le voy a responder
     datos = request.json
-    
+
     cursor = db.cursor()
 
-    cursor.execute('''INSERT INTO seccion(nombre, id_encuesta)
-        VALUE(%s, %s)''', (
-        datos['Nombre'],
-        id_encuesta,
-    ))
+    cursor.execute(
+        """UPDATE encuesta set nombre=%s, id_usuario=%s
+        where id_encuesta=%s""",
+        (
+            datos["Nombre"],
+            datos["id_usuario"],
+            id_encuesta,
+        ),
+    )
 
     db.commit()
-    
-    return jsonify({
 
-        "mensaje": "seccion almacenada correctamente"
-    })
+    return jsonify({"mensaje": "encuesta actualizada correctamente"})
 
-@app.get('/encuestas/secciones')
+
+@app.delete("/encuestas/<id_encuesta>")
+def eliminarEncuesta(id_encuesta):
+
+    cursor = db.cursor()
+    cursor.execute(
+        "DELETE FROM encuesta where id_encuesta=%s",
+        (id_encuesta,),
+    )
+
+    db.commit()
+
+    return jsonify({"mensaje": "encuesta eliminada correctamente"})
+
+
+@app.get("/encuestas/<id>")
+def unaEncuesta(id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM encuesta where id_encuesta=%s", [id])
+
+    encuesta = cursor.fetchall()
+
+    return jsonify(encuesta)
+
+
+@app.post("/secciones")
+def crearSeccion():
+    # request  => envia el cliente
+    # response => lo que le voy a responder
+    datos = request.json
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        """INSERT INTO seccion(nombre, id_encuesta)
+        VALUE(%s, %s)""",
+        (
+            datos["Nombre"],
+            datos["id_encuesta"],
+        ),
+    )
+
+    db.commit()
+
+    return jsonify({"mensaje": "seccion almacenada correctamente"})
+
+
+@app.get("/secciones")
 def listaSeccion():
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute('select * from seccion')
+    cursor.execute("select * from seccion")
 
     registros = cursor.fetchall()
 
     return jsonify(registros)
 
-@app.put('/encuestas/<id_encuesta>/secciones/<id_seccion>')
-def actualizarSeccion(id_encuesta, id_seccion):
 
-    datos=request.json
+@app.put("/secciones/<id_seccion>")
+def actualizarSeccion(id_seccion):
 
-    cursor = db.cursor()
-
-
-    cursor.execute('''UPDATE seccion set nombre=%s, id_encuesta=%s
-        where id_seccion=%s''',(
-            datos['Nombre'],
-            id_encuesta,
-            id_seccion
-        ))
-    
-    db.commit()
-
-    return jsonify({
-
-        "mensaje": "seccion actualizada correctamente"
-    })  
-
-@app.delete('/encuestas/<id_encuesta>/secciones/<id_seccion>')
-def eliminarSeccion(id_encuesta, id_seccion):
-
-
-    cursor = db.cursor()
-    cursor.execute('DELETE FROM seccion where id_encuesta=%s AND id_seccion=%s',(id_encuesta, id_seccion))
-
-
-    db.commit()
-
-    return jsonify({
-
-        "mensaje": "seccion eliminada correctamente"
-    })
-
-@app.post('/secciones/<id_seccion>/preguntas/<id_tipopregunta>')
-def crearPregunta(id_seccion, id_tipopregunta):
-    #request  => envia el cliente
-    #response => lo que le voy a responder
     datos = request.json
-    
+
     cursor = db.cursor()
 
-    cursor.execute('''INSERT INTO pregunta(nombre, id_seccion, id_tipopregunta)
-        VALUE(%s, %s, %s)''', (
-        datos['Nombre'],
-        id_seccion,
-        id_tipopregunta
-    ))
+    cursor.execute(
+        """UPDATE seccion set nombre=%s, id_encuesta=%s
+        where id_seccion=%s""",
+        (
+            datos["Nombre"],
+            datos["id_encuesta"],
+            id_seccion,
+        ),
+    )
 
     db.commit()
-    
-    return jsonify({
 
-        "mensaje": "pregunta almacenada correctamente"
-    })
+    return jsonify({"mensaje": "seccion actualizada correctamente"})
 
-@app.get('/secciones/preguntas')
+
+@app.delete("/secciones/<id_seccion>")
+def eliminarSeccion(id_seccion):
+
+    cursor = db.cursor()
+    cursor.execute(
+        "DELETE FROM seccion where id_seccion=%s",
+        (id_seccion,),
+    )
+
+    db.commit()
+
+    return jsonify({"mensaje": "seccion eliminada correctamente"})
+
+
+@app.get("/secciones/<id>")
+def unaSeccion(id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM seccion where id_seccion=%s", [id])
+
+    seccion = cursor.fetchall()
+
+    return jsonify(seccion)
+
+
+@app.post("/preguntas")
+def crearPregunta():
+    # request  => envia el cliente
+    # response => lo que le voy a responder
+    datos = request.json
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        """INSERT INTO pregunta(nombre, id_seccion, id_tipopregunta)
+        VALUE(%s, %s, %s)""",
+        (
+            datos["Nombre"],
+            datos["id_seccion"],
+            datos["id_tipopregunta"],
+        ),
+    )
+
+    db.commit()
+
+    return jsonify({"mensaje": "pregunta almacenada correctamente"})
+
+
+@app.get("/preguntas")
 def listaPregunta():
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute('select * from pregunta')
+    cursor.execute("select * from pregunta")
 
     registros = cursor.fetchall()
 
     return jsonify(registros)
 
-@app.put('/secciones/<id_seccion>/preguntas/<id_pregunta>')
-def actualizarPregunta(id_seccion, id_pregunta):
 
-    datos=request.json
+@app.put("/preguntas/<id_pregunta>")
+def actualizarPregunta(id_pregunta):
 
-    cursor = db.cursor()
-
-
-    cursor.execute('''UPDATE pregunta set nombre=%s, id_seccion=%s
-        where id_pregunta=%s''',(
-            datos['Nombre'],
-            id_seccion,
-            id_pregunta
-        ))
-    
-    db.commit()
-
-    return jsonify({
-
-        "mensaje": "pregunta actualizada correctamente"
-    })  
-
-@app.delete('/secciones/<id_seccion>/preguntas/<id_pregunta>')
-def eliminarPregunta(id_seccion, id_pregunta):
-
+    datos = request.json
 
     cursor = db.cursor()
-    cursor.execute('DELETE FROM pregunta where id_seccion=%s AND id_pregunta=%s',(id_seccion, id_pregunta))
 
+    cursor.execute(
+        """UPDATE pregunta set nombre=%s, id_seccion=%s , id_tipopregunta=%s
+        where id_pregunta=%s""",
+        (
+            datos["Nombre"],
+            datos["id_seccion"],
+            datos["id_tipopregunta"],
+            id_pregunta,
+        ),
+    )
 
     db.commit()
 
-    return jsonify({
+    return jsonify({"mensaje": "pregunta actualizada correctamente"})
 
-        "mensaje": "pregunta eliminada correctamente"
-    })
+
+@app.delete("/preguntas/<id_pregunta>")
+def eliminarPregunta(id_pregunta):
+
+    cursor = db.cursor()
+    cursor.execute(
+        "DELETE FROM pregunta where id_pregunta=%s",
+        (id_pregunta,),
+    )
+
+    db.commit()
+
+    return jsonify({"mensaje": "pregunta eliminada correctamente"})
+
+
+@app.get("/preguntas/<id>")
+def unaPregunta(id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM pregunta where id_pregunta=%s", [id])
+
+    pregunta = cursor.fetchall()
+
+    return jsonify(pregunta)
+
 
 app.run(debug=True)
